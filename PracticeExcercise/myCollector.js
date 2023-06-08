092d9e07b305e2abf9c2f80d74db77e11c38be1a
1Sarry <saronmeles4@gmail.com>
2023-06-08 18:02:39 +0300

    Doing HW

--- PracticeExcercise/myCollector.js
+++ PracticeExcercise/myCollector.js
@@ -2,12 +2,14 @@ const prod1 = require("./myFirst");
 const prod2 = require("./mySecond");
 const fs = require("fs");
 
-
-const data = fs.readFile('results.txt', 'utf8', function(err, data){
-    const prod3=prod1.myMultiplier(14)
-    if(err) console.log('Error', err);
-    else console.log(data , prod3 )
-})
+const prod3 = prod1.myMultiplier(20)
+// const data = fs.readFile('results.txt', 'utf8', function(err, data){
+    
+//     // if(err) console.log('Error', err);
+//     // else console.log(data , "(" + prod3 +")" )
+    
+// })
+console.log(prod3)
 //console.log(files)`(${data})`
 //prod1.myMultiplier(5);
 //prod2.myMultiplier(5);
