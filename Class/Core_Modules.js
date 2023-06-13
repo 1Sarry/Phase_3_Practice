5f09d55990bad65b66849d7302a85063aab59b48
1Sarry <saronmeles4@gmail.com>
2023-06-13 18:02:28 +0300

    Understand Serving static files _ html

--- Class/Core_Modules.js
+++ Class/Core_Modules.js
@@ -21,3 +21,27 @@ res.end("Hello There")
 x.listen(90, () =>{
     console.log("It is Listening")
 })
+
+
+
+
+// Express 
+const express = require ('express')
+
+const app = express()  // Call the function, const app by convention
+
+/*
+app obj has a lot of methods - corresponds to http method
+app.get()
+app.post()
+app.put()
+app.delete()
+
+*/ 
+
+
+app.get('/', (rea, res) =>{
+     res.send('Hello World')
+} ) // takes 2 args 1. path/url  2. callback fn which is will called when we get an http get request to our path/url(1st arg) 
+
+app.listen(3000, () => console.log('Listenting on port 300'))
--- nodeServerProject/static/apple-html-css-replica/buildServer.js
+++ nodeServerProject/static/apple-html-css-replica/buildServer.js
@@ -1,6 +1,8 @@
 const http = require("http");
 const fs = require("fs");
 const { dirname } = require("path");
+const url = require("url");
+const mimetypelookup = require("mime-types").lookup; // https://www.npmjs.com/package/mime-types
 
 // using readFileSync
 // const fileData = fs.readFileSync("./about.html");
@@ -16,18 +18,92 @@ const { dirname } = require("path");
 
 // Using readFile
 
-const server2 = http.createServer((req, res) => {
-  fs.readFile("./about.html", (err, fileData) => {
-    if (err) {
-      res.writeHead(404, { "Content-Type": "text/html" });
-      res.end("Page Not Found");
-    }
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write(fileData);
+// const server2 = http.createServer((req, res) => {
+//   fs.readFile("./about.html", (err, fileData) => {
+//     if (err) {
+//       res.writeHead(404, { "Content-Type": "text/html" });
+//       res.end("Page Not Found");
+//       console.log(req)
+//     }
+//     res.writeHead(200, { "Content-Type": "text/html" });
+//     res.write(fileData);
+//     res.end("This is it");
+//   });
+// });
+
+// server2.listen(8989, () => {
+//   console.log("Server is Listening to 8989...");
+// })
+
+/*
+Modify your request listener function in a way it would serve the "about.html"
+page when users request it on the browser
+■ Hint: You will need to import additional node modules to display your
+“about.html” page when users request it on the browser
+*/
+
+const server = http.createServer((req, res) => {
+  /*
+  // first - handle the request
+
+  //console.log(req);
+  //console.log(req.url) // req object always has the req.url data, so we can identify the requested file name
+  */
+  const parsedUrl = url.parse(req.url, true);
+  /*  
+   // to specifically identify the url (eg. parse /hhhh.html to /hhhh)
+  // allows us to extract the relevant parts of the url that we need to handle the request approprietly
+  //console.log(parsedUrl)
+
+  // - check the requested path - return the appropriet page or return error message
+  
+  */
+  let filePath = parsedUrl.path;
+  if (filePath == "/about.html") {
+    //console.log(__dirname)
+    //console.log(__filename)
+    var reqFile = __dirname + filePath;
+    const readF = fs.readFile(reqFile, (err, content) => {
+      if (err) {
+        res.writeHead(404);
+        res.end();
+      } else {
+        res.writeHead(200, { "content-type": "text/html" });
+        res.end(content);
+      }
+    });
+  } else {
+    res.write("Hello World");
     res.end();
-  });
+  }
 });
 
-server2.listen(8989, () => {
-  console.log("Server is Listening to 8989...");
-})
+server.listen(7070, () => {
+  console.log("I am Listening");
+});
+
+// Create the Server Object
+// const server = http.createServer(function (req, res) {
+//   console.log(req);
+// const parsedUrl = url.parse(req.url, true);
+// // console.log(parsedUrl);
+
+// // // Requested file name
+// let filePath = parsedUrl.path;
+// // console.log(filePath);
+// if (filePath == "/") {
+//   filePath = "/index.html";
+// }
+// var requestedFile = __dirname + "/public/" + filePath;
+// // console.log(requestedFile);
+// const readFile = fs.readFile(requestedFile, function (err, content) {
+//   if (err) {
+//     res.writeHead(404);
+//     res.end();
+//   } else {
+//     let mime = mimetypelookup(filePath);
+//     res.writeHead(200, { "content-type": mime });
+//     res.end(content);
+//   }
+// });
+// });
