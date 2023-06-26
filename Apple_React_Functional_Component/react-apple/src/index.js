0b82473976973ddc540f0a4e3e18b893830622d9
1Sarry <saronmeles4@gmail.com>
2023-06-26 03:18:25 +0300

    WOrking on promises

--- Apple_React_Functional_Component/react-apple/src/index.js
+++ Apple_React_Functional_Component/react-apple/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import "./bootstrap.css"
+//import "./bootstrap.css"
 import 'bootstrap/dist/js/bootstrap.min.js'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'jquery/dist/jquery.min.js'
