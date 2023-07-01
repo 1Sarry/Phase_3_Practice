23b83d7e8e6faed00163eaab6a716c1425e08a82
1Sarry <saronmeles4@gmail.com>
2023-07-01 17:57:39 +0300

    States Concept

---
+++ Class/class-based-states/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ClassState from './Components/ClassState';
+
+function App() {
+  return (
+    <div className="App">
+     <ClassState/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Class/class-based-states/src/Components/AnotherClass.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class AnotherClass extends Component {
+  render() {
+    return (
+      <div>
+        <p>{this.props.somevalue}</p>
+      </div>
+    )
+  }
+}
